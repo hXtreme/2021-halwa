17efb81864ab2f6862b496642447c0d5340bd37f
Harsh Parekh <harsh_parekh@outlook.com>
2021-11-23 20:51:27 -0500

    Auto Linting

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,60 +4,79 @@
 -- CIS 552, University of Pennsylvania
 
 -- | A small, applicative-based parsing library
-module Parser(Parser, doParse, get, eof, filter, 
-                          parse, parseFromFile, ParseError,
-                          satisfy, alpha, digit, upper, lower, space,
-                          char, string, int,
-                          chainl1, chainl, choice,
-                          between, sepBy1, sepBy) where
+module Parser
+  ( Parser,
+    doParse,
+    get,
+    eof,
+    filter,
+    parse,
+    parseFromFile,
+    ParseError,
+    satisfy,
+    alpha,
+    digit,
+    upper,
+    lower,
+    space,
+    char,
+    string,
+    int,
+    chainl1,
+    chainl,
+    choice,
+    between,
+    sepBy1,
+    sepBy,
+  )
+where
 
-import Prelude hiding (filter)
-
-import Control.Applicative (Alternative(..))
+import Control.Applicative (Alternative (..))
+import Control.Monad (guard)
 import Data.Char
+import Data.Foldable (asum)
 import qualified System.IO as IO
 import qualified System.IO.Error as IO
-import Control.Monad (guard)
-import Data.Foldable (asum)
-
+import Prelude hiding (filter)
 
 -- definition of the parser type
-newtype Parser a = P { doParse :: String -> Maybe (a, String) }
+newtype Parser a = P {doParse :: String -> Maybe (a, String)}
 
 instance Functor Parser where
   fmap :: (a -> b) -> Parser a -> Parser b
-  fmap f p = P $ \s -> do (c, cs) <- doParse p s
-                          return (f c, cs)
-                          
+  fmap f p = P $ \s -> do
+    (c, cs) <- doParse p s
+    return (f c, cs)
+
 instance Applicative Parser where
-  pure :: a -> Parser a 
-  pure x    = P $ \s -> Just (x,s)
+  pure :: a -> Parser a
+  pure x = P $ \s -> Just (x, s)
 
   (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-  p1 <*> p2 = P $ \ s -> do (f, s') <- doParse p1 s
-                            (x,s'') <- doParse p2 s'
-                            return (f x, s'')
-                            
+  p1 <*> p2 = P $ \s -> do
+    (f, s') <- doParse p1 s
+    (x, s'') <- doParse p2 s'
+    return (f x, s'')
+
 instance Alternative Parser where
-  
   empty :: Parser a
   empty = P $ const Nothing
-  
+
   (<|>) :: Parser a -> Parser a -> Parser a
   p1 <|> p2 = P $ \s -> doParse p1 s `firstJust` doParse p2 s
 
 instance Monad Parser where
   return = pure
   (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-  p >>= f = P $ \s -> do (x, s') <- doParse p s
-                         doParse (f x) s'
+  p >>= f = P $ \s -> do
+    (x, s') <- doParse p s
+    doParse (f x) s'
 
 -- | Combine two Maybe values together, producing the first
 -- successful result
 firstJust :: Maybe a -> Maybe a -> Maybe a
 firstJust (Just x) _ = Just x
-firstJust Nothing  y = y
-
+firstJust Nothing y = y
 
 -- | Return the next character from the input
 get :: Parser Char
@@ -68,17 +87,15 @@ get = P $ \case
 -- | This parser *only* succeeds at the end of the input.
 eof :: Parser ()
 eof = P $ \case
-  []  -> Just ((),[]) 
-  _:_ -> Nothing
+  [] -> Just ((), [])
+  _ : _ -> Nothing
 
 -- | Filter the parsing results by a predicate
 filter :: (a -> Bool) -> Parser a -> Parser a
-filter f p = P $ \s ->  do 
-                         (c , cs) <- doParse p s
-                         guard (f c)
-                         return (c , cs)
-
-
+filter f p = P $ \s -> do
+  (c, cs) <- doParse p s
+  guard (f c)
+  return (c, cs)
 
 ---------------------------------------------------------------
 ---------------------------------------------------------------
@@ -91,9 +108,8 @@ type ParseError = String
 -- give it a type similar to other Parsing libraries.
 parse :: Parser a -> String -> Either ParseError a
 parse parser str = case doParse parser str of
-    Nothing    -> Left  "No parses"
-    Just (a,_) -> Right a
-
+  Nothing -> Left "No parses"
+  Just (a, _) -> Right a
 
 -- | parseFromFile p filePath runs a string parser p on the input
 -- read from filePath using readFile. Returns either a
@@ -101,13 +117,14 @@ parse parser str = case doParse parser str of
 parseFromFile :: Parser a -> String -> IO (Either ParseError a)
 parseFromFile parser filename = do
   IO.catchIOError
-    (do
+    ( do
         handle <- IO.openFile filename IO.ReadMode
         str <- IO.hGetContents handle
-        pure $ parse parser str)
-    (\e ->
-        pure $ Left $ "Error:" ++ show e)
-    
+        pure $ parse parser str
+    )
+    ( \e ->
+        pure $ Left $ "Error:" ++ show e
+    )
 
 -- | Return the next character if it satisfies the given predicate
 satisfy :: (Char -> Bool) -> Parser Char
@@ -135,22 +152,21 @@ string = foldr (\c p -> (:) <$> char c <*> p) (pure "")
 int :: Parser Int
 int = read <$> ((++) <$> string "-" <*> some digit <|> some digit)
 
-
 -- | Parses one or more occurrences of @p@ separated by binary operator
 -- parser @pop@.  Returns a value produced by a /left/ associative application
 -- of all functions returned by @pop@.
 -- See the end of the `Parsers` lecture for explanation of this operator.
 chainl1 :: Parser a -> Parser (a -> a -> a) -> Parser a
-p `chainl1` pop = foldl comb <$> p <*> rest where
-   comb x (op,y) = x `op` y
-   rest = many ((,) <$> pop <*> p)
+p `chainl1` pop = foldl comb <$> p <*> rest
+  where
+    comb x (op, y) = x `op` y
+    rest = many ((,) <$> pop <*> p)
 
 -- | @chainl p pop x@ parses zero or more occurrences of @p@, separated by @pop@.
 -- If there are no occurrences of @p@, then @x@ is returned.
 chainl :: Parser b -> Parser (b -> b -> b) -> b -> Parser b
 chainl p pop x = chainl1 p pop <|> pure x
 
-  
 -- | Combine all parsers in the list (sequentially)
 choice :: [Parser a] -> Parser a
 choice = asum -- equivalent to: foldr (<|>) empty
@@ -159,7 +175,6 @@ choice = asum -- equivalent to: foldr (<|>) empty
 --   @close@. Only the value of @p@ is pureed.
 between :: Parser open -> Parser a -> Parser close -> Parser a
 between open p close = open *> p <* close
-
 
 -- | @sepBy p sep@ parses zero or more occurrences of @p@, separated by @sep@.
 --   Returns a list of values returned by @p@.
@@ -172,4 +187,3 @@ sepBy1 :: Parser a -> Parser sep -> Parser [a]
 sepBy1 p sep = (:) <$> p <*> many (sep *> p)
 
 ---------------------------------------------
-
