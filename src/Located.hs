e35429f9c55877735ab2e06f4caf693831a4ea11
Harsh Parekh <harsh_parekh@outlook.com>
2021-11-24 03:21:05 -0500

    Use data instead of newtype.

--- src/Located.hs
+++ src/Located.hs
@@ -2,38 +2,38 @@ module Located where
 
 import Control.Monad.Cont (Monad)
 
-newtype Location
-  = Location (Int, Int) -- (line, column)
+data Location
+  = Location Int Int -- (line, column)
   deriving (Eq, Show)
 
 -- | Row number of the location.
 row :: Location -> Int
-row (Location (l, _)) = l
+row (Location l _) = l
 
 -- | Column number of the location.
 column :: Location -> Int
-column (Location (_, c)) = c
+column (Location _ c) = c
 
 -- | The location that follows after a character.
 locationAfter :: Located Char -> Location
-locationAfter (L (l, c)) = case c of
-  '\n' -> Location (row l + 1, 1)
-  _ -> Location (row l, column l + 1)
+locationAfter (L l c) = case c of
+  '\n' -> Location (row l + 1) 1
+  _ -> Location (row l) (column l + 1)
 
 instance Ord Location where
   l1 `compare` l2 = case row l1 `compare` row l2 of
     EQ -> column l1 `compare` column l2
     x -> x
 
-newtype Located a
-  = L (Location, a)
+data Located a
+  = L Location a
   deriving (Eq, Show)
 
 loc :: Located a -> Location
-loc (L (l, _)) = l
+loc (L l _) = l
 
 val :: Located a -> a
-val (L (_, a)) = a
+val (L _ a) = a
 
 locateAt :: Location -> a -> Located a
 locateAt l a = L (l, a)
@@ -42,7 +42,7 @@ instance Functor Located where
   fmap f l = L (loc l, f . val $ l)
 
 instance Applicative Located where
-  pure a = L (Location (1, 1), a)
+  pure a = L (Location 1 1, a)
 
   {-
   Alternatively consider:
@@ -53,4 +53,4 @@ instance Applicative Located where
 instance Monad Located where
   return = pure
   (>>=) :: Located a -> (a -> Located b) -> Located b
-  (L (l, a)) >>= f = f a
+  L l a >>= f = f a
