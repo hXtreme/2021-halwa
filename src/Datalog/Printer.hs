77aa4bb0979cf6d2831b13b9f76a84d580f06f41
aoch1019 <andrewoch95@gmail.com>
2021-12-01 12:06:17 -0500

    First draft for printing declarations

---
+++ src/Datalog/Printer.hs
@@ -0,0 +1,45 @@
+module Datalog.Printer where
+
+import Datalog.Program
+import Datalog.Declaration
+import Datalog.Rule
+import Datalog.Fact
+import Datalog.Disjunction
+import Datalog.Query
+import Common (Type)
+
+import Data.List
+
+
+-- data Program = Program
+--   { declarations :: [Declaration],
+--     rules :: [Rule],
+--     facts :: [Fact],
+--     disjunction :: [Disjunction],
+--     queries :: [Query]
+--   }
+--   deriving (Eq, Show)
+
+main :: IO ()
+main = putStrLn "Hello, World!"
+
+printProgram :: Program -> [IO ()]
+printProgram pr = do
+                    printDeclarations (declarations pr)
+                    -- printRules (rules pr)
+                    -- printFacts (facts pr)
+                    -- printDisjunctions (disjunctions pr)
+                    -- printQueries (queries pr)
+                    -- return ()
+
+printDeclarations :: [Declaration] -> [IO ()]
+printDeclarations ds = map (\x -> printDecl x) ds
+
+printDecl :: Declaration -> IO ()
+printDecl d = putStr (".decl" ++ (predicate d) ++ (printArgs (typeToString (argTypes d))))
+
+typeToString :: [Type] -> [String]
+typeToString ts = map (\x -> show x) ts
+
+printArgs :: [String] -> String
+printArgs s = "(" ++ (intercalate ", " s) ++ ")"
