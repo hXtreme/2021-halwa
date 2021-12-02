1b6bb6b4c49597f929220a36275db7282c36d253
aoch1019 <andrewoch95@gmail.com>
2021-12-02 17:55:05 -0500

    Created prettyPrint for Query

--- src/Datalog/Query.hs
+++ src/Datalog/Query.hs
@@ -1,5 +1,9 @@
 module Datalog.Query where
 
-import Datalog.Atom (Atom)
+import Datalog.Atom
+import PrettyPrinter
 
 newtype Query = Query {query :: Atom} deriving (Eq, Show)
+
+instance Pretty Query where
+  prettyPrint f = "output " ++ predicate (query f)
