4a946c5732c66af45d7e6d50edcf483385661fd7
aoch1019 <andrewoch95@gmail.com>
2021-12-03 13:45:28 -0500

    Wrote a test for Program prettyPrint

--- src/Datalog/Program.hs
+++ src/Datalog/Program.hs
@@ -1,10 +1,16 @@
 module Datalog.Program where
 
-import Datalog.Declaration (Declaration)
-import Datalog.Rule (Rule)
-import Datalog.Fact (Fact)
-import Datalog.Disjunction (Disjunction)
-import Datalog.Query (Query)
+import Datalog.Declaration
+import Datalog.Rule
+import Datalog.Fact
+import Datalog.Disjunction
+import Datalog.Query
+import Datalog.Constant
+import Datalog.Argument
+import Datalog.Variable
+import Datalog.Atom
+import Datalog.Literal
+import Common
 import PrettyPrinter
 
 -- https://souffle-lang.github.io/examples
@@ -21,9 +27,9 @@ data Program = Program
   deriving (Eq, Show)
 
 instance Pretty Program where
-  prettyPrint (Program de r f di q) = prettyDeclarations ++ "\n" ++ prettyRules ++ 
-                  "\n" ++ prettyFacts ++ "\n" ++ prettyDisjunctions ++
-                  "\n" ++ prettyQueries
+  prettyPrint (Program de r f di q) = prettyDeclarations ++ "\n\n" ++ prettyRules ++ 
+                  "\n\n" ++ prettyFacts ++ "\n\n" ++ prettyDisjunctions ++
+                  "\n\n" ++ prettyQueries
                   where
                     prettyDeclarations = prettyPrint (NewLineSeparatedList de)
                     prettyRules = prettyPrint (NewLineSeparatedList r)
@@ -31,3 +37,23 @@ instance Pretty Program where
                     -- prettyDisjunctions = prettyPrint (NewLineSeparatedList di)
                     prettyDisjunctions = "Disjunction temp"
                     prettyQueries = prettyPrint (NewLineSeparatedList q)
+
+
+test :: IO ()
+test = let d1 = Declaration {Datalog.Declaration.predicate="hi", argTypes=[Common.Symbol, Common.Boolean]}
+           d2 = Declaration {Datalog.Declaration.predicate="yo", argTypes=[Common.Integer, Common.String]}
+           const1 = Datalog.Constant.Integer 3
+           const2 = Datalog.Constant.Symbol "Symb"
+           var1 = Datalog.Variable.Variable {name="var"}
+           a1 = Atom {Datalog.Atom.predicate="test", args=[Constant const1, Datalog.Argument.Variable var1]}
+           fact1 = Fact {Datalog.Fact.head=a1}
+           lit1 = Pos a1
+           rule1 = Rule {Datalog.Rule.head=a1, body=[lit1, lit1]}
+           query1 = Query {query=a1}
+           declarations = [d1, d2]
+           rules = [rule1, rule1]
+           facts = [fact1, fact1, fact1]
+           queries = [query1, query1]
+           disjunctions = []
+           program1 = Program {declarations=declarations, rules=rules, Datalog.Program.facts=facts, queries=queries, disjunction=disjunctions}
+        in putStrLn $ prettyPrint program1
