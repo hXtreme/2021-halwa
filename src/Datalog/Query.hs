27ea117a7d1590fa4e5038a8492e5c098631f4e5
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-05 18:33:39 -0500

    Implement Parseable for Query.

--- src/Datalog/Query.hs
+++ src/Datalog/Query.hs
@@ -1,5 +1,12 @@
 module Datalog.Query where
 
 import Datalog.Atom (Atom)
+import Parseable (Parseable (parser))
+import qualified Parser as P
 
 newtype Query = Query {query :: Atom} deriving (Eq, Show)
+
+instance Parseable Query where
+  parser = Query <$> (keyWordQuery *> parser)
+    where
+      keyWordQuery = P.stringP "query"
