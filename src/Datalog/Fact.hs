5522204725c388052879de0139dd58f656d9cd5c
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-05 18:39:12 -0500

    Implement Parseable for Fact.

--- src/Datalog/Fact.hs
+++ src/Datalog/Fact.hs
@@ -1,5 +1,9 @@
 module Datalog.Fact where
 
 import Datalog.Atom (Atom)
+import Parseable (Parseable (parser))
 
 newtype Fact = Fact {head :: Atom} deriving (Eq, Show)
+
+instance Parseable Fact where
+  parser = Fact <$> parser
