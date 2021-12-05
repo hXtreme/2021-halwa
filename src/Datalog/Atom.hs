380c4122149282da649998392390f36e0e3bc9ff
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-05 18:25:15 -0500

    Implement Parseable for Atom and Declaration.

--- src/Datalog/Atom.hs
+++ src/Datalog/Atom.hs
@@ -1,9 +1,19 @@
 module Datalog.Atom where
 
+import Control.Applicative
 import Datalog.Argument (Argument)
+import qualified Datalog.Common as DL.Common
+import Parseable (Parseable (parser))
+import qualified Parser as P
 
 data Atom = Atom
   { predicate :: String,
     args :: [Argument]
   }
   deriving (Eq, Show)
+
+instance Parseable Atom where
+  parser = Atom <$> predicate <*> args
+    where
+      predicate = P.filter DL.Common.isNotKeyWord $ P.wsP P.snakeCaseWord
+      args = many parser
--- src/Datalog/Declaration.hs
+++ src/Datalog/Declaration.hs
@@ -1,9 +1,20 @@
 module Datalog.Declaration where
 
 import Common (Type)
+import Control.Applicative
+import qualified Datalog.Common as DL.Common
+import Parseable (Parseable (parser))
+import qualified Parser as P
 
 data Declaration = Declaration
   { predicate :: String,
     argTypes :: [Type]
   }
   deriving (Eq, Show)
+
+instance Parseable Declaration where
+  parser = Declaration <$> (keyWordDecl *> name) <*> argTypes
+    where
+      keyWordDecl = P.stringP "decl"
+      name = P.filter DL.Common.isNotKeyWord $ P.wsP P.snakeCaseWord
+      argTypes = P.parensP (P.sepBy parser P.commaP) <|> pure []
