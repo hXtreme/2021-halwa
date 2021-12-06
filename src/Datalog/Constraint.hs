d6e4318c00fa6eecfdbe5769079c8092a3bac3ae
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-06 07:58:31 -0500

    Implement Parseable for Constraint.

--- src/Datalog/Constraint.hs
+++ src/Datalog/Constraint.hs
@@ -1,9 +1,16 @@
 module Datalog.Constraint where
 
 import Common (BinOP, UnOP)
+import Control.Applicative
 import Datalog.Argument (Argument)
+import Parseable (Parseable (parser))
 
 data Constraint
-  = BinaryConstraint BinOP Argument Argument
+  = BinaryConstraint Argument BinOP Argument
   | UnaryConstraint UnOP Argument
   deriving (Eq, Show)
+
+instance Parseable Constraint where
+  parser =
+    BinaryConstraint <$> parser <*> parser <*> parser
+      <|> UnaryConstraint <$> parser <*> parser
