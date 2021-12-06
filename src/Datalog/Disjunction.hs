7658da2c00d40e5ebfe73fe978842a7e1daeab7c
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-06 08:28:48 -0500

    Implement Datalog/Disjunction

--- src/Datalog/Disjunction.hs
+++ src/Datalog/Disjunction.hs
@@ -1,5 +1,13 @@
 module Datalog.Disjunction where
 
 import Datalog.Fact (Fact)
+import Parseable (Parseable (parser))
+import Control.Applicative
+import qualified Parser as P
 
 newtype Disjunction = Disjunction {facts :: [Fact]} deriving (Eq, Show)
+
+instance Parseable Disjunction where
+  parser = Disjunction <$> fewFacts
+    where
+        fewFacts = P.filter ((>= 2) . length) (P.sepBy parser P.semicolonP)
