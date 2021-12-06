34b98bfa54dd8a5063f008997e17e15899ea6663
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-06 08:08:44 -0500

    Make Query parser stricter.

--- src/Datalog/Query.hs
+++ src/Datalog/Query.hs
@@ -1,12 +1,13 @@
 module Datalog.Query where
 
-import Datalog.Atom (Atom)
+import Datalog.Atom (Atom (args))
 import Parseable (Parseable (parser))
 import qualified Parser as P
 
 newtype Query = Query {query :: Atom} deriving (Eq, Show)
 
 instance Parseable Query where
-  parser = Query <$> (keyWordQuery *> parser)
+  parser = Query <$> (keyWordQuery *> relation)
     where
+      relation = P.filter (null . args) parser
       keyWordQuery = P.stringP "query"
