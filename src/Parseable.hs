1b792a1624b8c3e944f34f901f5c17acc6396fd5
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-06 07:51:01 -0500

    Fix capitalization.

--- src/Parseable.hs
+++ src/Parseable.hs
@@ -8,7 +8,7 @@ class Parseable a where
   parser :: Parser a
 
 instance Parseable Bool where
-  parser = P.constP "True" True <|> P.constP "False" False
+  parser = P.constP "true" True <|> P.constP "false" False
 
 instance Parseable Int where
   parser = P.wsP P.int
