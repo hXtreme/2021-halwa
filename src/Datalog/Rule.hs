a0b1b1d181adcd0a5e93787521e3e27ae2195670
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-06 08:14:06 -0500

    Implement Parseable for Rule.

--- src/Datalog/Rule.hs
+++ src/Datalog/Rule.hs
@@ -2,9 +2,16 @@ module Datalog.Rule where
 
 import Datalog.Atom (Atom)
 import Datalog.Literal (Literal)
+import Parseable (Parseable (parser))
+import qualified Parser as P
 
 data Rule = Rule
   { head :: Atom,
     body :: [Literal]
   }
   deriving (Eq, Show)
+
+instance Parseable Rule where
+  parser = Rule <$> parser <*> (P.stringP ":-" *> bodyP)
+    where
+      bodyP = P.sepBy1 parser P.commaP
