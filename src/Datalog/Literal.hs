5c49d772ad1ccb1e5e5b3acc805ba1f78416008a
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-06 07:59:50 -0500

    Implement Parseable for Literal.

--- src/Datalog/Literal.hs
+++ src/Datalog/Literal.hs
@@ -1,10 +1,20 @@
 module Datalog.Literal where
 
+import Control.Applicative
 import Datalog.Atom (Atom)
 import Datalog.Constraint (Constraint)
+import Parseable (Parseable (parser))
+import qualified Parser as P
 
 data Literal
   = Pos Atom
   | Neg Atom
   | Constraint Constraint
   deriving (Eq, Show)
+
+instance Parseable Literal where
+  parser = negP <|> posP <|> constraintP
+    where
+      negP = Neg <$> (P.stringP "!" *> parser)
+      posP = Pos <$> parser
+      constraintP = Constraint <$> parser
