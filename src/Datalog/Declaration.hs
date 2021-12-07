0d4e02a19da3badf86f7b5c1ed7d35f5304a26a2
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-07 06:01:08 -0500

    Implement Pretty for Declaration.

--- src/Datalog/Declaration.hs
+++ src/Datalog/Declaration.hs
@@ -5,6 +5,7 @@ import Control.Applicative
 import qualified Datalog.Common as DL.Common
 import Parseable (Parseable (parser))
 import qualified Parser as P
+import Pretty (CommaSeparatedList (CSL), Pretty (pretty))
 
 data Declaration = Declaration
   { predicate :: String,
@@ -18,3 +19,10 @@ instance Parseable Declaration where
       keyWordDecl = P.stringP "decl"
       name = P.filter DL.Common.isNotKeyWord $ P.wsP P.snakeCaseWord
       argTypes = P.parensP (P.sepBy parser P.commaP) <|> pure []
+
+instance Pretty Declaration where
+  pretty (Declaration pred argTypes) =
+    "decl " ++ pred
+      ++ "("
+      ++ pretty (CSL argTypes)
+      ++ ")"
