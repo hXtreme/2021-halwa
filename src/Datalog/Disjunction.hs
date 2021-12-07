93dc9346a760c80469e01f7cea47113e3c0b5c89
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-07 06:38:51 -0500

    Implement Pretty for Disjunction

--- src/Datalog/Disjunction.hs
+++ src/Datalog/Disjunction.hs
@@ -1,13 +1,18 @@
 module Datalog.Disjunction where
 
+import Control.Applicative
+import Data.List (intercalate)
 import Datalog.Fact (Fact)
 import Parseable (Parseable (parser))
-import Control.Applicative
 import qualified Parser as P
+import Pretty (Pretty (pretty))
 
 newtype Disjunction = Disjunction {facts :: [Fact]} deriving (Eq, Show)
 
 instance Parseable Disjunction where
   parser = Disjunction <$> fewFacts
     where
-        fewFacts = P.filter ((>= 2) . length) (P.sepBy parser P.semicolonP)
+      fewFacts = P.filter ((>= 2) . length) (P.sepBy parser P.semicolonP)
+
+instance Pretty Disjunction where
+  pretty (Disjunction fs) = intercalate "; " (map pretty fs)
