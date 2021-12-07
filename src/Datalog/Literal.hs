a790cf6b13ad4157eefed0d3dcc68b612cc2ab8c
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-07 05:57:35 -0500

    Implement Pretty for Literal.

--- src/Datalog/Literal.hs
+++ src/Datalog/Literal.hs
@@ -5,6 +5,7 @@ import Datalog.Atom (Atom)
 import Datalog.Constraint (Constraint)
 import Parseable (Parseable (parser))
 import qualified Parser as P
+import Pretty (Pretty (pretty))
 
 data Literal
   = Pos Atom
@@ -18,3 +19,8 @@ instance Parseable Literal where
       negP = Neg <$> (P.stringP "!" *> parser)
       posP = Pos <$> parser
       constraintP = Constraint <$> parser
+
+instance Pretty Literal where
+  pretty (Pos a) = pretty a
+  pretty (Neg a) = "!" <> pretty a
+  pretty (Constraint c) = pretty c
