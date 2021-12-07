ff5da29de595c63d529d55372a79191cf98c863c
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-07 06:05:39 -0500

    Implement Pretty for Query.

--- src/Datalog/Query.hs
+++ src/Datalog/Query.hs
@@ -3,6 +3,7 @@ module Datalog.Query where
 import Datalog.Atom (Atom (args))
 import Parseable (Parseable (parser))
 import qualified Parser as P
+import Pretty (Pretty (pretty))
 
 newtype Query = Query {query :: Atom} deriving (Eq, Show)
 
@@ -11,3 +12,6 @@ instance Parseable Query where
     where
       relation = P.filter (null . args) parser
       keyWordQuery = P.stringP "query"
+
+instance Pretty Query where
+  pretty (Query atom) = "query " ++ pretty atom
