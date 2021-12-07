1c6d2503227e8d12ace1d319de05026efceb7ec1
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-07 05:52:21 -0500

    Implement Pretty for Constraint.

--- src/Datalog/Constraint.hs
+++ src/Datalog/Constraint.hs
@@ -1,9 +1,10 @@
 module Datalog.Constraint where
 
-import Common (BinOP, UnOP)
+import Common (BinOP, UnOP (Pos))
 import Control.Applicative
 import Datalog.Argument (Argument)
 import Parseable (Parseable (parser))
+import Pretty (Pretty (pretty))
 
 data Constraint
   = BinaryConstraint Argument BinOP Argument
@@ -14,3 +15,10 @@ instance Parseable Constraint where
   parser =
     BinaryConstraint <$> parser <*> parser <*> parser
       <|> UnaryConstraint <$> parser <*> parser
+
+instance Pretty Constraint where
+  pretty (BinaryConstraint a op b) =
+    "(" ++ pretty a ++ " " ++ pretty op ++ " " ++ pretty b <> ")"
+  pretty (UnaryConstraint op a) = case op of
+    Pos -> pretty a
+    _ -> "(" ++ pretty op <> pretty a ++ ")"
