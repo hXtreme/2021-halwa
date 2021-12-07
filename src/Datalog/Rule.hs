2c34de3d3c9c3430723499f09df004099717635e
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-07 06:10:57 -0500

    Implement Pretty for Rule.

--- src/Datalog/Rule.hs
+++ src/Datalog/Rule.hs
@@ -4,6 +4,7 @@ import Datalog.Atom (Atom)
 import Datalog.Literal (Literal)
 import Parseable (Parseable (parser))
 import qualified Parser as P
+import Pretty (CommaSeparatedList (CSL), Pretty (pretty))
 
 data Rule = Rule
   { head :: Atom,
@@ -15,3 +16,6 @@ instance Parseable Rule where
   parser = Rule <$> parser <*> (P.stringP ":-" *> bodyP)
     where
       bodyP = P.sepBy1 parser P.commaP
+
+instance Pretty Rule where
+  pretty (Rule head body) = pretty head <> " :- " <> pretty (CSL body)
