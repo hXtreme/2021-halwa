457adacee47faa05eb3b003bd43c65f620cf0062
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-07 06:02:44 -0500

    Implement Pretty for Fact.

--- src/Datalog/Fact.hs
+++ src/Datalog/Fact.hs
@@ -2,8 +2,12 @@ module Datalog.Fact where
 
 import Datalog.Atom (Atom)
 import Parseable (Parseable (parser))
+import Pretty (Pretty (pretty))
 
 newtype Fact = Fact {head :: Atom} deriving (Eq, Show)
 
 instance Parseable Fact where
   parser = Fact <$> parser
+
+instance Pretty Fact where
+  pretty (Fact a) = pretty a
