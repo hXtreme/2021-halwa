c972f6a28fd9f3027fe17cbc1954a4db72535def
Harsh Parekh <harsh_parekh@outlook.com>
2021-12-15 13:02:20 -0500

    Add example nums.
    Shows that we can have disjunctions.

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,12 @@
 module Main where
 
 import qualified Datalog.Program as DL
+import qualified Even as Demo.Even
+import qualified FrogEngine.Lib as Engine
+import qualified Nums as Demo.Nums
 import Parseable (parseFromFile)
 import Pretty (pretty)
 import System.Environment (getArgs)
-import qualified Even as Demo.Even
-import qualified FrogEngine.Lib as Engine
 
 loadFile :: FilePath -> IO DL.Program
 loadFile fileName = do
@@ -27,6 +28,7 @@ main = do
   case args of
     [fileName] -> cli fileName
     ["demo", "evens", num] -> runEvensDemo num
+    ["demo", "nums", num] -> runNumsDemo num
     _ -> putStrLn "Usage: stack run <datalog-file>"
 
 runEvensDemo :: String -> IO ()
@@ -35,4 +37,12 @@ runEvensDemo num = do
   where
     base = read num :: Int
     program = Demo.Even.evens base
+    execResult = Engine.exec program
+
+runNumsDemo :: String -> IO ()
+runNumsDemo num = do
+  putStrLn $ Engine.prettyResults execResult
+  where
+    base = read num :: Int
+    program = Demo.Nums.nums' base
     execResult = Engine.exec program
